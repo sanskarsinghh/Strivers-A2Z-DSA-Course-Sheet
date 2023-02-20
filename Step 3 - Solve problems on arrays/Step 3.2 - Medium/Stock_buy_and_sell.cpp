0c099bb5df601ad5e45acccb021daee7e4cbb9d3
sanskarsingh <sanskarsingh.college@gmail.com>
2023-02-20 12:48:41 +0530

    first commit

---
+++ Step 3 - Solve problems on arrays/Step 3.2 - Medium/Stock_buy_and_sell.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solution(int arr[], int n) // Time complexity = O(N^2)
+{
+    int maxpro = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] > arr[i])
+                maxpro = max(arr[j] - arr[i], maxpro);
+        }
+    }
+    return maxpro;
+}
+int solution1(int arr[], int n) //Time complexity = O(N)
+{
+    int maxprice = 0;
+    int minprice = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        minprice = min(arr[i], minprice);
+        maxprice = max(maxprice, arr[i]- minprice);
+    }
+    return maxprice;
+}
+int main()
+{
+    int arr[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    // cout << solution(arr, n);
+    cout << solution1(arr, n);
+    return 0;
+}
--- Step 3 - Solve problems on arrays/Step 3.2 - Medium/Subarray_with_a_give_sum.cpp
+++ Step 3 - Solve problems on arrays/Step 3.2 - Medium/Subarray_with_a_give_sum.cpp
@@ -5,15 +5,15 @@ void solution(int arr[], int n, int k)
     int start = 0, end = -1, sum = 0;
     while (start < n)
     {
-        while ((end + 1) < n && (sum + arr[end + 1] == k))
+        while ((end + 1 < n) && (sum + arr[end + 1] <= k))
         {
             sum += arr[++end];
 
             if (sum == k)
             {
-                for (int i = start; i < end; i++)
+                for (int i = start; i <= end; i++)
                 {
-                    cout << arr[i] << " " << endl;
+                    cout << arr[i] << " ";
                 }
             }
         }
@@ -23,9 +23,9 @@ void solution(int arr[], int n, int k)
 }
 int main()
 {
-    int arr[] = {1, 9, 3, 7};
+    int arr[] = {1, 9, 3, 7, 2, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int k = 8;
+    int k = 10;
     solution(arr, n, k);
     return 0;
 }
